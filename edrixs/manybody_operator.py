17d136a7ddee01e18da1adb9ada5c124a55636bc
Yilin Wang <wangyilin2015@gmail.com>
2019-05-15 14:43:49 -0400

    add new general function to build manybody operators

--- edrixs/manybody_operator.py
+++ edrixs/manybody_operator.py
@@ -111,7 +111,8 @@ def four_fermion(umat, lb, rb=None, tol=1E-10):
 
     .. math::
 
-        <F_l|\\sum_{ij}U_{ijkl}\\hat{f}_{i}^{\\dagger}\\hat{f}_{j}^{\\dagger}\\hat{f}_{k}\\hat{f}_{l}|F_r>
+        <F_l|\\sum_{ij}U_{ijkl}\\hat{f}_{i}^{\\dagger}\\hat{f}_{j}^{\\dagger}
+        \\hat{f}_{k}\\hat{f}_{l}|F_r>
 
     Parameters
     ----------
@@ -171,6 +172,101 @@ def four_fermion(umat, lb, rb=None, tol=1E-10):
             jcfg = indx[tuple(tmp_basis)]
             if jcfg != -1:
                 hmat[jcfg, icfg] += umat[lorb, korb, jorb, iorb] * s1 * s2 * s3 * s4
+    return hmat
+
+
+def build_opers(nfermion, coeff, lb, rb=None, tol=1E-10):
+    """
+    Build matrix form of many-body operators in the given Fock basis,
+
+    .. math::
+
+        <F_{l}|\\sum_{ij}E_{ij}\\hat{f}_{i}^{\\dagger}\\hat{f}_{j}|F_{r}>
+
+    or
+
+    .. math::
+
+        <F_l|\\sum_{ij}U_{ijkl}\\hat{f}_{i}^{\\dagger}\\hat{f}_{j}^{\\dagger}
+        \\hat{f}_{k}\\hat{f}_{l}|F_r>
+
+    Parameters
+    ----------
+    nfermion: int
+        Number of fermion operators. Options can only be 2 or 4 now.
+    coeff: array-like
+        The coefficients.
+
+        - if nfermion=2, coeff should be at least 2-dimension, and the last 2-dimension is
+          the matrix of coefficients of an operator. For examples,
+
+          - coeff.shape = (3, 10, 10), means 3 operators with :math:`10 \\times 10`
+            coefficients matrix.
+
+          - coeff.shape = (2, 3, 10, 10), means :math:`2 \\times 3=6` operators with
+            :math:`10 \\times 10` coefficients matrix.
+
+        - if nfermion=4, coeff should be at least 4-dimension, and the last 4-dimension is
+          the rank-4 tensor of coefficients of an operator. For examples,
+
+          - coeff.shape = (3, 10, 10, 10, 10), means 3 operators with
+            :math:`10 \\times 10 \\times 10 \\times 10` coefficients tensor.
+
+          - coeff.shape = (2, 3, 10, 10, 10, 10), means :math:`2 \\times 3=6` operators with
+            :math:`10 \\times 10 \\times 10 \\times 10` coefficients tensor.
+    lb: list or array
+        Left fock basis :math:`<F_{l}|`.
+    rb: list of array
+        Right fock basis :math:`|F_{r}>`.
+        rb = lb if rb is None
+    tol: float (default: 1E-10)
+        Only consider the elements of emat that are larger than tol.
+
+    Returns
+    -------
+    hmat: array-like
+        At least 2-dimension and the last 2-dimension is matrix form of operators,
+        For examples,
+
+        - if nfermion=2, coeff.shape=(2, 3, 10, 10), hmat.shape=(2, 3, len(lb), len(rb))
+
+        - if nfermion=4, coeff.shape=(2, 3, 10, 10, 10, 10), hmat.shape=(2, 3, len(lb), len(rb))
+    """
+    if nfermion not in [2, 4]:
+        raise Exception("nfermion is not 2 or 4")
+    nl = len(lb)
+    if rb is None:
+        nr = nl
+    else:
+        nr = len(rb)
+    coeff = np.array(coeff, order='C')
+    if nfermion == 2:
+        dim = coeff.shape
+        if len(dim) < 2:
+            raise Exception("Dimension of coeff should be at least 2 when nfermion=2")
+        elif len(dim) == 2:
+            hmat = two_fermion(coeff, lb, rb, tol)
+        else:
+            tot = np.prod(dim[0:-2])
+            hmat_tmp = np.zeros((tot, nl, nr), dtype=np.complex)
+            coeff_tmp = coeff.reshape((tot, dim[-2], dim[-1]))
+            for i in range(tot):
+                hmat_tmp[i] = two_fermion(coeff_tmp[i], lb, rb, tol)
+            hmat = hmat_tmp.reshape((dim[0:-2], nl, nr))
+    if nfermion == 4:
+        dim = coeff.shape
+        if len(dim) < 4:
+            raise Exception("Dimension of coeff should be at least 4 when nfermion=4")
+        elif len(dim) == 4:
+            hmat = four_fermion(coeff, lb, rb, tol)
+        else:
+            tot = np.prod(dim[0:-4])
+            hmat_tmp = np.zeros((tot, nl, nr), dtype=np.complex)
+            coeff_tmp = coeff.reshape((tot, dim[-4], dim[-3], dim[-2], dim[-1]))
+            for i in range(tot):
+                hmat_tmp[i] = four_fermion(coeff_tmp[i], lb, rb, tol)
+            hmat = hmat_tmp.reshape((dim[0:-2], nl, nr))
+
     return hmat
 
 
