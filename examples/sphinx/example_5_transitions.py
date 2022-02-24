fa0da610f62d3c58d629194edf5011e9c5492c18
Mark Dean <mpmdean@gmail.com>
2022-02-24 16:14:32 -0500

    fix ex 5

--- examples/sphinx/example_5_transitions.py
+++ examples/sphinx/example_5_transitions.py
@@ -3,8 +3,9 @@
 X-ray transitions
 ================================================================================
 This example explains how to calculate x-ray transition amplitudes between
-specific orbital and spin states. We take the case of a cuprate with a
-:math:`d_{x^2-y^2}` ground state and compute the angular dependence of spin-flip
+specific orbital and spin states. We take the case of a cuprate which has one
+hole in the :math:`d_{x^2-y^2}` orbital and a spin ordering direction along the
+in-plane diagaonal direction and compute the angular dependence of spin-flip
 and non-spin-flip processes.
 
 This case was chosen because the eigenvectors in question are simple enough
@@ -25,86 +26,83 @@ import scipy
 # Eigenvectors
 # ------------------------------------------------------------------------------
 # Let us start by determining the eigenvectors involved in the transitions.
-# If we wanted to, we could build and diagonalize a suitable Hamiltonian, but
-# it will be more convenient and pedagogical to write a function that generates
-# them directly. Using hole language and the real harmonic basis we can generate
-# the eignvector by making an array of zeroes and setting the relevant
-# :code:`orbital_index` to one. The array can then be transformed into the
-# complex harmonic basis. The spin direction can be set using a vector
+# The spin direction can be set using a vector
 # :math:`\vec{B}` to represent a magnetic field in terms of generalized spin
 # operator :math:`\tilde{\sigma}=\vec{B}\cdot\sigma` based on the Pauli matrices
-# :math:`\sigma`. A full spin-orbital
-# eignvector with an arbitrary spin direction :math:`\Psi` can be
-# generated in terms of orbital eigenvector :math:`\psi` as
-#
-#     .. math::
-#        \Psi^{\uparrow} = \frac{1}{\sqrt{2}}
-#        \left[U_- \psi^{\uparrow} + D_- \psi^{\uparrow}\right]
-#
-# where :math:`U_-` and :math:`D_-` are the first components of the two
-# eigenvectors of :math:`\tilde{\sigma}`.
+# :math:`\sigma`. Let's put the spin along the :math:`[1, 1, 0]` direction
+# and formuate the problem in the hole basis.
+# For one particle, we know that the Hamiltonian will be diagonal in the real
+# harmonic basis.
+# We can generate the required eigenvectors by making a diagonal
+# matrix, transforming it to the required
+# complex harmonic basis (as is standard for EDRIXS) and diagonalizing it.
+# As long as the crystal field splitting is much larger than the magnetic
+# field, the eigenvectors will be independent of the exact values of both
+# these parameters.
 
+B = 1e-3*np.array([1, 1, 0])
+cf_splitting = 1e3
+zeeman = sum(s*b for s, b in zip(edrixs.get_spin_momentum(2), B))
+dd_levels = np.array([energy for dd_level in cf_splitting*np.arange(5)
+                      for energy in [dd_level]*2], dtype=complex)
+emat_rhb = np.diag(dd_levels)
+emat = edrixs.cb_op(emat_rhb, edrixs.tmat_r2c('d', ispin=True)) + zeeman
+_, eigenvectors = np.linalg.eigh(emat)
 
-def make_eigenvector(orbital_index, B, case='d'):
-    tmat_r2c = edrixs.tmat_r2c(case)
-    norb = tmat_r2c.shape[0]
-    eigenvector_rhb = np.zeros(norb, dtype=complex)
-    eigenvector_rhb[orbital_index] = 1
-    psi = np.dot(eigenvector_rhb, tmat_r2c)
-
-    Spauli = edrixs.get_pauli()
-    Emat_spin = sum(B[i]*Spauli[i] for i in range(3))
-    _, v = scipy.linalg.eigh(Emat_spin)
-    U, D = v[:, 0], v[:, 1]
-
-    Psi = np.zeros(2*norb, dtype=complex)
-    Psi[::2] = psi*U[0]
-    Psi[1::2] = psi*D[0]
-    return Psi
+def get_eigenvector(orbital_index, spin_index):
+    return eigenvectors[:, 2*orbital_index + spin_index]
 
 
 ################################################################################
-# Let's put the spin along the :math:`[1 1 0]` direction and
-# recall from the :ref:`sphx_glr_auto_examples_example_1_crystal_field.py`
+# Let's examine the :math:`d_{x^2-y^2}` orbital first. Recall from the
+# :ref:`sphx_glr_auto_examples_example_1_crystal_field.py`
 # example that edrixs uses the standard orbital order of
-# :math:`d_{3z^2-r^2}, d_{xz}, d_{yz}, d_{x^2-y^2}, d_{xy}`, so we want the
+# :math:`d_{3z^2-r^2}, d_{xz}, d_{yz}, d_{x^2-y^2}, d_{xy}`. So we want
 # :code:`orbital_index = 3` element. Using this, we can build spin-up and -down
 # eigenvectors.
 orbital_index = 3
-B = np.array([1, 1, 0])
 
-groundstate_vector = make_eigenvector(orbital_index, B)
-excitedstate_vector = make_eigenvector(orbital_index, -B)
-
-################################################################################
-# Transition operators
-# ------------------------------------------------------------------------------
-# Here we are considering a :math:`L_3`-edge :math:`2p_{3/2} \rightarrow 3d`
-# transition. We can read this matrix from the edrixs database, keeping in
-# mind that there are in fact three operations for :math:`x, y,` & :math:`z` directions.
-# We will also need the :math:`3d \rightarrow 2p_{3/2}` operation, which we can
-# generate as the conjugate transpose.
-
-Tmat = edrixs.get_trans_oper('dp32')
-Tmat_dag = np.array([np.conj(Tpol).T for Tpol in Tmat])
+groundstate_vector = get_eigenvector(orbital_index, 0)
+excitedstate_vector = get_eigenvector(orbital_index, 1)
 
 ################################################################################
-# Scattering matrix
+# Transition operators and scattering matrix
 # ------------------------------------------------------------------------------
+# Here we are considering the :math:`L_3`-edge. This means
+# a :math:`2p_{3/2} \rightarrow 3d`
+# absoprtion transition and a :math:`2p_{3/2} \rightarrow 3d`
+# emission transition. We can read the relevant matrix from the edrixs database,
+# keeping in mind that there are in fact three operations for
+# :math:`x, y,` & :math:`z` directions. Note that edrixs provides operators
+# in electron notation. If we define :math:`D` as the transition operator in
+# electron language, "math:`D^\dagger` is the operator in the hole language
+# we are using for this example.
 # The angular dependence of a RIXS transition can be conveniently described
 # using the scattering matrix, which is a :math:`3\times3` element object that
 # specifies the transition amplitude for each incoming and outgoing x-ray
-# polarization.
+# polarization. Correspondingly, we have
+#
+#      .. math::
+#        \begin{equation}
+#        \mathcal{F}=\sum_n\langle f|D|n\rangle\langle n|D^{\dagger}|g\rangle
+#        \end{equation}.
+#
+# In matrix form this is
+#
+#      .. math::
+#        \begin{equation}
+#        \mathcal{F}(m,n)=\{f^{\dagger} \cdot D(m)\} \cdot \{D^{\dagger}(n) \cdot g\}
+#        \end{equation}.
 
+D_Tmat = edrixs.get_trans_oper('dp32')
 
 def get_F(vector_i, vector_f):
     F = np.zeros((3, 3), dtype=complex)
     for i in range(3):
         for j in range(3):
-            F[i, j] = np.dot(np.dot(Tmat_dag[i], vector_f),
-                             np.dot(vector_i, Tmat[j]))
+            F[i, j] = np.dot(np.dot(np.conj(vector_f.T), D_Tmat[i]),
+                             np.dot(np.conj(D_Tmat[j].T), vector_i))
     return F
-
 
 ################################################################################
 # Using this function, we can obtain non-spin-flip (NSF) and spin-flip (SF)
@@ -157,6 +155,39 @@ ax.legend()
 ax.set_xlabel(r'Theta ($^\circ$)')
 ax.set_ylabel('Relative intensity')
 plt.show()
+
+################################################################################
+# Run through orbitals
+# ------------------------------------------------------------------------------
+# For completeness, let's look at transitions from :math:`x^2-y^2` to all other
+# orbitals.
+fig, axs = plt.subplots(5, 1, figsize=(7, 7),
+                        sharex=True, sharey=True)
+
+orbitals = ['$d_{3z^2-r^2}$', '$d_{xz}$', '$d_{yz}$',
+            '$d_{x^2-y^2}$', '$d_{xy}$']
+orbital_order = [4, 1, 2, 0, 3]
+
+plot_index = 0
+for ax, orbital_index in zip(axs, orbital_order):
+    for spin_index, spin_label in zip([0, 1], ['NSF', 'SF']):
+        excitedstate_vector = get_eigenvector(orbital_index, spin_index)
+        F = get_F(groundstate_vector, excitedstate_vector)
+        for alpha, pol_label in zip([0, np.pi/2], [r'$\pi$', r'$\sigma$']):
+            Is = np.array([get_I(thin, alpha, F) for thin in thins])
+            ax.plot(thins, Is*10, label=f'{pol_label} {spin_label}',
+                    color=f'C{plot_index%4}')
+            plot_index += 1
+    ax.legend(title=orbitals[orbital_index], bbox_to_anchor=(1.1, 1),
+              loc="upper left", fontsize=8)
+
+
+axs[-1].set_xlabel(r'$\theta$ ($^\circ$)')
+axs[2].set_ylabel('Scattering intensity')
+
+fig.subplots_adjust(hspace=0, left=.3, right=.6)
+plt.show()
+
 ##############################################################################
 #
 # .. rubric:: Footnotes
