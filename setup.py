369c2d267649c588ada56ddcfe6231f49f49e908
Joseph Kleinhenz <kleinhenz.joseph@gmail.com>
2022-03-06 12:51:39 -0800

    wip allow setuptools to drive cmake build

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
-from os import path
-from setuptools import find_packages
+import os
+import platform
+import subprocess
 import sys
 import versioneer
+from setuptools import find_packages, setup, Extension
+from setuptools.command.build_ext import build_ext
+from pprint import pprint
 
-# It needs f2py, https://www.numpy.org/devdocs/f2py/distutils.html
-from numpy.distutils.core import Extension
-from numpy.distutils.core import setup
+# Command line flags forwarded to CMake (for debug purpose)
+cmake_cmd_args = []
+for f in sys.argv:
+    if f.startswith('-D'):
+        cmake_cmd_args.append(f)
+
+for f in cmake_cmd_args:
+    sys.argv.remove(f)
 
 # NOTE: This file must remain Python 2 compatible for the foreseeable future,
 # to ensure that we error out properly for people with outdated setuptools
@@ -27,12 +36,12 @@ pip install --upgrade pip
     )
     sys.exit(error)
 
-here = path.abspath(path.dirname(__file__))
+here = os.path.abspath(os.path.dirname(__file__))
 
-with open(path.join(here, "README.rst"), encoding="utf-8") as readme_file:
+with open(os.path.join(here, "README.rst"), encoding="utf-8") as readme_file:
     readme = readme_file.read()
 
-with open(path.join(here, "requirements.txt")) as requirements_file:
+with open(os.path.join(here, "requirements.txt")) as requirements_file:
     # Parse requirements.txt, ignoring any commented-out lines.
     requirements = [
         line
@@ -40,19 +49,87 @@ with open(path.join(here, "requirements.txt")) as requirements_file:
         if not line.startswith("#")
     ]
 
-# Python interface to call fortran subroutines
-if "linux" in sys.platform:
-    extra_link_args = ["-shared"]
-else:
-    extra_link_args = []
-ext_fortran = Extension(
-    name="edrixs.fedrixs", sources=["src/pyapi.f90"], extra_link_args=extra_link_args
-)
+def _get_env_variable(name, default='OFF'):
+    if name not in os.environ.keys():
+        return default
+    return os.environ[name]
+
+# adapted from https://martinopilia.com/posts/2018/09/15/building-python-extension.html
+class CMakeExtension(Extension):
+    def __init__(self, name, cmake_lists_dir='.', sources=[], **kwargs):
+        Extension.__init__(self, name, sources=sources, **kwargs)
+        self.cmake_lists_dir = os.path.abspath(cmake_lists_dir)
+
+class cmake_build_ext(build_ext):
+    def build_extensions(self):
+        # Ensure that CMake is present and working
+        try:
+            out = subprocess.check_output(['cmake', '--version'])
+        except OSError:
+            raise RuntimeError('Cannot find CMake executable')
+
+        for ext in self.extensions:
+
+            # NOTE CMakeExtension("edrixs.foo") results in extdir pointing to edrixs subdir (foo is ignored)
+            extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
+            cfg = 'Debug' if _get_env_variable('EDRIXS_DEBUG') == 'ON' else 'Release'
+
+            cmake_args = [
+                '-DCMAKE_BUILD_TYPE=%s' % cfg,
+                # Ask CMake to place the resulting library in the directory
+                # containing the extension
+                '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir),
+                # Other intermediate static libraries are placed in a
+                # temporary build directory instead
+                '-DCMAKE_ARCHIVE_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), self.build_temp),
+                # Install executables in edrixs/bin so that they are accessible and can be wrapped (not sure if this is right approach)
+                # see discussion here https://discuss.python.org/t/packaging-a-compiled-executable/9954/
+                '-DCMAKE_RUNTIME_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), os.path.join(extdir, "bin")),
+                # Hint CMake to use the same Python executable that
+                # is launching the build, prevents possible mismatching if
+                # multiple versions of Python are installed
+                # FIXME this hint doesn't seem to work on newer cmake
+                '-DPYTHON_EXECUTABLE={}'.format(sys.executable),
+                # Add other project-specific CMake arguments if needed
+                # ...
+            ]
+
+            # We can handle some platform-specific settings at our discretion
+            if platform.system() == 'Windows':
+                plat = ('x64' if platform.architecture()[0] == '64bit' else 'Win32')
+                cmake_args += [
+                    # These options are likely to be needed under Windows
+                    '-DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE',
+                    '-DCMAKE_RUNTIME_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir),
+                ]
+                # Assuming that Visual Studio and MinGW are supported compilers
+                if self.compiler.compiler_type == 'msvc':
+                    cmake_args += [
+                        '-DCMAKE_GENERATOR_PLATFORM=%s' % plat,
+                    ]
+                else:
+                    cmake_args += [
+                        '-G', 'MinGW Makefiles',
+                    ]
+
+            cmake_args += cmake_cmd_args
+
+            if not os.path.exists(self.build_temp):
+                os.makedirs(self.build_temp)
+
+            # Config
+            subprocess.check_call(['cmake', ext.cmake_lists_dir] + cmake_args,
+                                  cwd=self.build_temp)
+
+            # Build
+            subprocess.check_call(['cmake', '--build', '.', '--config', cfg],
+                                  cwd=self.build_temp)
+
+
 
 setup(
     name="edrixs",
     version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
     description="An open source toolkit for simulating RIXS spectra based on ED",
     long_description=readme,
     author="Brookhaven National Lab",
@@ -82,5 +159,6 @@ setup(
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
     ],
-    ext_modules=[ext_fortran],
+    ext_modules=[CMakeExtension("edrixs.fedrixs")], # edrixs.foo puts build outputs under edrixs subdir
+    cmdclass={'build_ext' : cmake_build_ext}
 )
