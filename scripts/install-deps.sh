5d1699ba5e76635eb18730ec21ce0de69806bb7c
Max Rakitin <mrakitin@bnl.gov>
2023-09-15 15:56:56 -0400

    Comment-out wheels building step for now

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -17,8 +17,8 @@ sudo apt-get install -y \
 # installation problems if out of date.
 python -m pip install --upgrade pip "setuptools<=65.5.*" numpy
 
-# Generate .whl file.
-python setup.py bdist_wheel
+# # Generate .whl file.
+# python setup.py bdist_wheel
 
 # Install this package and the packages listed in requirements.txt.
 pip install -v .
