b22208e2bca9b4ef3b758e23e6689094448998d8
Max Rakitin <mrakitin@bnl.gov>
2024-02-27 18:31:19 -0500

    PKG: always use `python -m pip install ...`

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -22,6 +22,9 @@ fi
 # These packages are installed in the base environment but may be older
 # versions. Explicitly upgrade them because they often create
 # installation problems if out of date.
+which python
+python -VV
+
 python -m pip install --upgrade pip setuptools wheel numpy
 
 # # Generate .whl file.
@@ -29,10 +32,10 @@ python setup.py sdist bdist_wheel
 ls -la dist/
 
 # Install this package and the packages listed in requirements.txt.
-pip install dist/edrixs-*-cp${PYTHON_VERSION_NODOT}*.whl
+python -m pip install dist/edrixs-*-cp${PYTHON_VERSION_NODOT}*.whl
 
 # Install extra requirements for running tests and building docs.
-pip install -r requirements-dev.txt
+python -m pip install -r requirements-dev.txt
 
 # List the depencencies
-pip list
+python -m pip list
