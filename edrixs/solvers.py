86ca312c71c588b2d8d1ddc1db6fa6beccd8cbf5
lajerm <40722884+lajerm@users.noreply.github.com>
2024-06-05 15:30:22 -0400

    Update solvers.py

--- edrixs/solvers.py
+++ edrixs/solvers.py
@@ -553,10 +553,10 @@ def rixs_1v1c_py(eval_i, eval_n, trans_op, ominc, eloss, *,
                 for n in range(npol):
                     F_mag[:, :] += np.conj(polvec_f[m]) * F_fi[m, n] * polvec_i[n]
            
-           excInd = 0
-           if (skipGS):
-               excInd = len(gs_list) 
-           for m, igs in enumerate(gs_list):
+            excInd = 0
+            if (skipGS):
+                excInd = len(gs_list) 
+            for m, igs in enumerate(gs_list):
                 for n in range(excInd,len(eval_i)):
                     rixs[i, :, j] += (
                         prob[m] * np.abs(F_mag[n, igs])**2 * gamma_final / np.pi /
