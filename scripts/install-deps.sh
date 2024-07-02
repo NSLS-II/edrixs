37838b62b79f734e080e6368ba72cf269ecd830e
lajerm <40722884+lajerm@users.noreply.github.com>
2024-07-01 23:19:36 -0400

    Update install-deps.sh

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -31,7 +31,7 @@ python -VV
 python -m pip install --upgrade pip setuptools wheel numpy
 
 # # Generate .whl file.
-python setup.py sdist bdist_wheel
+python setup.py sdist bdist_wheel --plat-name=manylinux2014
 ls -la dist/
 
 # Install this package and the packages listed in requirements.txt.
